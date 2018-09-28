6dcfa57bac5e04bb83ed8516ca95ff3053007649
RamsesSolano <rsolano-c@3cinteractive.com>
2018-09-28 18:13:25 -0500

    Initial Commit

---
+++ 02-Dados/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-Dados
+//
+//  Created by Ramses Solano Ramirez on 28/09/18.
+//  Copyright © 2018 Emk Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 02-DadosTests/_2_DadosTests.swift
@@ -0,0 +1,34 @@
+//
+//  _2_DadosTests.swift
+//  02-DadosTests
+//
+//  Created by Ramses Solano Ramirez on 28/09/18.
+//  Copyright © 2018 Emk Soft. All rights reserved.
+//
+
+import XCTest
+@testable import _2_Dados
+
+class _2_DadosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
